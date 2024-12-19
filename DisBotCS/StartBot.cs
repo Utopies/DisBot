b6a57975f736a05d3c848756feab7d64cf63938f
Utopies / Daniil Klimenko <etotgus0@gmail.com>
2024-12-20 00:42:32 +0300

    Bot Conneting

--- DisBotCS/StartBot.cs
+++ DisBotCS/StartBot.cs
@@ -1,7 +1,32 @@
-﻿class StartBot
+﻿using DSharpPlus;
+
+sealed class StartBot
 {
-    public static void Main(string[] args)
+    private static DiscordClient  Client { get; set; }
+    
+    public static async Task Main(string[] args)
     {
+        Client = new DiscordClient(SetConfig());
+        await Client.ConnectAsync();
+        await Task.Delay(-1);
+    }
+
+    private static DiscordConfiguration SetConfig()
+    {
+        var disConfig = new DiscordConfiguration()
+        {
+            Intents = DiscordIntents.All,
+            Token = ReadToken(),
+            TokenType = TokenType.Bot,
+            AutoReconnect = true,
+        };
         
+        return disConfig;
+    }
+    
+    private static string ReadToken()
+    {
+        string token = File.ReadAllText("Token.txt");
+        return token;
     }
 }
